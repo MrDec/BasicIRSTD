015647641f34cf29ab9adb2ccff62aa3793b87eb
Duan Weiwei <35191838+MrDec@users.noreply.github.com>
2024-06-12 17:19:25 +0800

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,12 +1,12 @@
-from model.Unet.model_Unet import U_Net as Unet
-from model.ACM.model_ALCnet import ASKCResNetFPN as ALCNet
-from model.ACM.model_ACM import ASKCResUNet as ACM
-from model.DNANet.model_DNANet import DNANet as DNANet
+# from model.Unet.model_Unet import U_Net as Unet
+# from model.ACM.model_ALCnet import ASKCResNetFPN as ALCNet
+# from model.ACM.model_ACM import ASKCResUNet as ACM
+# from model.DNANet.model_DNANet import DNANet as DNANet
 # from model.ISNet.model_ISNet import ISNet as ISNet
 # from model.RISTDnet.model_RISTDnet import RISTDnet as RISTDnet
-from model.UIUNet.model_UIUNet import UIUNET as UIUNet
+# from model.UIUNet.model_UIUNet import UIUNET as UIUNet
 # from BasicIRSTD.model.ISTDUNet.model_ISTDUNet_1 import ISTDU_Net as ISTDU_Net
-from model.RDIAN.model_RDIAN import RDIAN as RDIAN
+# from model.RDIAN.model_RDIAN import RDIAN as RDIAN
 from model.AGPCNet.network import AGPCNet as AGPCNet
-from model.RPCANet.net import RPCANet as RPCANet
-from model.MTUNet.MTUNet import res_UNet as MTUNet
+# from model.RPCANet.net import RPCANet as RPCANet
+# from model.MTUNet.MTUNet import res_UNet as MTUNet
