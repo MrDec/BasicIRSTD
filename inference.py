da14084c80220aef0a636da6937f638f9bc53e9f
Duan Weiwei <35191838+MrDec@users.noreply.github.com>
2024-06-12 17:26:37 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -8,14 +8,13 @@ from metrics import *
 import os
 import time
 from tqdm import tqdm
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 parser = argparse.ArgumentParser(description="PyTorch BasicIRSTD Inference without mask")
-parser.add_argument("--model_names", default=['AGPCNet'], nargs='+',  
+parser.add_argument("--model_names", default=['ACM', 'ALCNet','DNANet', 'ISNet', 'RDIAN', 'ISTDU-Net'], nargs='+',  
                     help="model_name: 'ACM', 'ALCNet', 'DNANet', 'ISNet', 'UIUNet', 'RDIAN', 'ISTDU-Net', 'U-Net', 'RISTDnet'")
-parser.add_argument("--pth_dirs", default=['PRCV/AGPCNet_64.pth.tar'], nargs='+',  help="checkpoint dir, default=None or ['NUDT-SIRST/ACM_400.pth.tar','NUAA-SIRST/ACM_400.pth.tar']")
-parser.add_argument("--dataset_dir", default='/home/public', type=str, help="train_dataset_dir")
-parser.add_argument("--dataset_names", default=['PRCV'], nargs='+', 
+parser.add_argument("--pth_dirs", default=None, nargs='+',  help="checkpoint dir, default=None or ['NUDT-SIRST/ACM_400.pth.tar','NUAA-SIRST/ACM_400.pth.tar']")
+parser.add_argument("--dataset_dir", default='./datasets', type=str, help="train_dataset_dir")
+parser.add_argument("--dataset_names", default=['NUAA-SIRST', 'NUDT-SIRST', 'IRSTD-1K'], nargs='+', 
                     help="dataset_name: 'NUAA-SIRST', 'NUDT-SIRST', 'IRSTD-1K', 'SIRST3', 'NUDT-SIRST-Sea'")
 parser.add_argument("--img_norm_cfg", default=None, type=dict,
                     help="specific a img_norm_cfg, default=None (using img_norm_cfg values of each dataset)")
